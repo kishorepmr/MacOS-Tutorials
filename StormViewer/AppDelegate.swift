544955813513568163a29fe2cc67db29f8a3ca88
kponmuth <kponmuth@cisco.com>
2020-08-02 11:30:27 +0530

    Initial Commit

---
+++ StormViewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  StormViewer
+//
+//  Created by kponmuth on 02/08/20.
+//  Copyright © 2020 kponmuth. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
